0fcf9feb2775ea95ffb21c24ccff9a3ddbcadc18
Emmanuel Utomi <emmanuelutomi@gmail.com>
2015-01-29 21:58:09 -0600

    WildKernel
    Conflicts:
    	arch/arm/configs/Sultan_skyrocket_defconfig
    	arch/arm/configs/wild_defconfig

---
+++ flashable/tools/updatewild.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#DO NOT EDIT FILE - AUTO GENERATED FOR hercules
+latestversion=012
+latestdate=141214
+latestdateliteral='December 14 2014'
+latestDL=http://forum.xda-developers.com/showthread.php?t=2765196
+input="$1"
+if [[ -z "$input" ]]
+	then
+	exit 1
+fi
+
+if [[ "$input" == "latestversion" ]]
+	then
+	echo $latestversion
+
+elif [[ "$input" == "latestdate" ]]
+	then
+	echo $latestdate
+
+elif [[ "$input" == "latestDL" ]]
+	then
+	echo $latestDL
+elif [[ "$input" == "latestdateliteral" ]]
+	then
+	echo $latestdateliteral
+fi
+
+# echo ${!input} Apparently not all roms have bash installed ._.
