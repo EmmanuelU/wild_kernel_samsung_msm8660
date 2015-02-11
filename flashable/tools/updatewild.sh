4cc6587ac26f05c46385a6dd41b0496fe1db4227
Emmanuel Utomi <emmanuelutomi@gmail.com>
2015-02-10 22:35:38 -0600

    Update WK Version
    Conflicts:
    	arch/arm/configs/wild_defconfig
    	build
    	flashable/tools/updatewild.sh

--- flashable/tools/updatewild.sh
+++ flashable/tools/updatewild.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 #DO NOT EDIT FILE - AUTO GENERATED FOR hercules
+<<<<<<< HEAD
 latestversion=012
 latestdate=141214
 latestdateliteral='December 14 2014'
+=======
+latestversion=014-LP
+latestdate=150210
+latestdateliteral='February 10 2015'
+>>>>>>> 1c91924... Update WK Version
 latestDL=http://forum.xda-developers.com/showthread.php?t=2765196
 input="$1"
 if [[ -z "$input" ]]
